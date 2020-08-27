839557937fb30508cffb76de56ca90fc899b2b36
ekremtas <ekremtas589@gmail.com>
2020-08-27 13:48:59 +0300

    ReactNative app Created over Expo & react-native-deck-swiper Library Added & sample data created in data.js file & Swiper in App.js created

--- App.js
+++ App.js
@@ -1,12 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, Platform } from "react-native";
+import { Dimensions } from "react-native";
+
+import data from "./data";
+import Swiper from "react-native-deck-swiper";
+
+const Card = ({ card }) => (
+  <View style={styles.card}>
+    <Image source={{ uri: card.image }} style={styles.cardImage} />
+  </View>
+);
 
 export default function App() {
+  const [index, setIndex] = useState(0);
+  const onSwiped = () => {
+    setIndex((index + 1) % data.length);
+  };
+  const [wanted, setWanted] = useState([]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Swiper
+        backgroundColor={"transparent"}
+        cards={data}
+        cardIndex={index}
+        renderCard={(card) => <Card key={card.id} card={card} />}
+        useViewOverflow={Platform.OS === "ios"}
+        onSwiped={onSwiped}
+        stackSize={4}
+        stackScale={10}
+        stackSeparation={35}
+        disableTopSwipe
+        disableBottomSwipe
+        animateOverlayLabelsOpacity
+        animateCardOpacity
+        inputOverlayLabelsOpacityRangeX={[
+          -Dimensions.get("window").width / 15,
+          -Dimensions.get("window").width / 16,
+          0,
+          Dimensions.get("window").width / 16,
+          Dimensions.get("window").width / 15,
+        ]}
+        overlayOpacityHorizontalThreshold={-Dimensions.get("window").width / 1}
+        infinite
+        onSwipedLeft={(index, item) => {
+          console.log(index, item.name, " malzemesini almadı.");
+          setWanted(wanted.filter((x) => x.id !== item.id));
+        }}
+        onSwipedRight={(index, item) => {
+          console.log(index, item.name, " malzemesini satın aldı");
+          setWanted([...wanted, item]);
+        }}
+        overlayLabels={{
+          left: {
+            title: "İlgilenmiyorum",
+            style: {
+              label: {
+                borderRadius: 5,
+                borderWidth: 5,
+                width: "100%",
+                paddingLeft: "50%",
+                height: "75%",
+                color: "red",
+                fontSize: 24,
+                borderColor: "red",
+              },
+              wrapper: {
+                flezDirection: "column",
+                alignItems: "flex-end",
+                justifyContent: "flex-start",
+                marginTop: 20,
+                marginLeft: -20,
+              },
+            },
+          },
+          right: {
+            title: "İstiyorum",
+            style: {
+              label: {
+                borderRadius: 5,
+                borderWidth: 5,
+                width: "100%",
+                height: "75%",
+                color: "red",
+                fontSize: 24,
+                borderColor: "green",
+                color: "green",
+                fontSize: 24,
+              },
+              wrapper: {
+                flezDirection: "column",
+                alignItems: "flex-start",
+                justifyContent: "flex-start",
+                marginTop: 20,
+                marginLeft: 20,
+              },
+            },
+          },
+        }}
+      />
+      <View style={styles.BottomInfoContainer}>
+        <Text style={{ marginTop: 500 }}>{JSON.stringify(wanted)}</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +108,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#099D72",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  card: {
+    flex: 0.8,
+    borderRadius: 8,
+    shadowRadius: 25,
+    shadowColor: "#000",
+    shadowOpacity: 0.08,
+    shadowOffset: { width: 0, height: 0 },
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  BottomInfoContainer:{
+  },
+  cardImage: { width: "95%", flex: 1, resizeMode: "contain" },
 });
---
+++ data.js
@@ -0,0 +1,72 @@
+export default [
+  {
+    id: "0",
+    price: "₺20",
+    name: "Ice Tea Şeftali",
+    image:
+      "https://www.bypapoburger.com/image/cache/catalog/urunler/BYP-1030/BYP-1030-1620x1080.png",
+  },
+  {
+    id: "2",
+    price: "₺36",
+    name: "Oba Ayçiçek YAğı",
+    image:
+      "https://ayb.akinoncdn.com/products/2019/09/04/25814/fdddebe4-3e8c-452d-bc75-6272dec47153_size780x780_quality60_cropCenter.jpg",
+  },
+  {
+    id: "3",
+    price: "₺8.50",
+    name: "Temizlik Bezi",
+    image:
+      "https://www.birlesiktemizlik.com/Uploads/UrunResimleri/buyuk/Suff-SFFSMT01-Microfiber-Temizlik-Bezi-3-9d46.jpg",
+  },
+  {
+    id: "4",
+    price: "₺24",
+    name: "CocaCola",
+    image:
+      "https://migros-dali-storage-prod.global.ssl.fastly.net/sanalmarket/product/08010001/08010001-50b349-1650x1650.jpg",
+  },
+  {
+    id: "5",
+    price: "₺52",
+    name: "Sıvı Sabun",
+    image:
+      "https://productimages.hepsiburada.net/s/26/1500/10149244174386.jpg",
+  },
+  {
+    id: "6",
+    price: "₺46",
+    name: "Bulgur",
+    image:
+      "https://migros-dali-storage-prod.global.ssl.fastly.net/sanalmarket/product/01089951/01089951-8bf828-1650x1650.jpg",
+  },
+  {
+    id: "7",
+    price: "₺144",
+    name: "Havlu KAğıt",
+    image:
+      "https://ayb.akinoncdn.com/products/2019/10/14/27798/cca9cc64-3a9c-4b54-b0a7-ce60ec4b05fe_size780x780_quality60_cropCenter.jpg",
+  },
+  {
+    id: "8",
+    price: "₺39.50",
+    name: "Mısır GEvreği",
+    image:
+      "https://migros-dali-storage-prod.global.ssl.fastly.net/sanalmarket/product/05091409/nestle-corn-flakes-misir-gevregi-450-gr-f150a0-1650x1650.jpg",
+  },
+  {
+    id: "9",
+    price: "₺30",
+    name: "Vileda",
+    image:
+      "https://st3.myideasoft.com/idea/dc/01/myassets/products/389/vileda-easy-clean-pedalli-temizlik-seti-1000123720-400.jpg?revision=1524558688",
+  },
+  {
+    id: "1",
+    price: "₺52",
+    name: "Omo Active Deterjan",
+    image:
+      "https://ayb.akinoncdn.com/products/2019/02/04/15555/df45ad82-c69d-41d5-858a-ca0645959669_size780x780_quality60_cropCenter.jpg",
+  }
+];
